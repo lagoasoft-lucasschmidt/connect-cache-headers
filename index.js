6c52eda0f48c4c6238a6bc942b276de87e7afa3b
Ole Michaelis <Ole.Michaelis@googlemail.com>
2015-03-12 13:44:12 +0100

    allow additional cache-control directives

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-module.exports = function(age, private) {
-  var cacheType = "public";
-  if (private){
-    cacheType = "private";
+// http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.9
+module.exports = function(age, private, additionalDirectives) {
+  private = typeof private !== 'undefined' ? private : true;
+  additionalDirectives = typeof additionalDirectives !== 'undefined' ? additionalDirectives : [];
+
+  var directives = [];
+  directives.push((private) ? 'private' : 'public');
+  directives.push("max-age=" + age);
+  for (var i = 0; i < additionalDirectives.length; i++) {
+    directives.push(additionalDirectives[i]);
   }
-  var control = cacheType + ", max-age=" + age;
   return function(req, res, next) {
-    res.setHeader("Cache-Control", control);
+    res.setHeader("Cache-Control", directives.join(', '));
     res.removeHeader("Pragma");
     res.removeHeader("Expires");
     return next();
